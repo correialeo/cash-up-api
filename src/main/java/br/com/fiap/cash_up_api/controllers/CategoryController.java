672374edf855479f80edf93dfd04c95b9cdf95c6
correialeo <leandroo774@outlook.com>
2025-02-27 09:36:15 -0300

    endpoints de criar, buscar pelo id e remover criados

--- src/main/java/br/com/fiap/cash_up_api/controllers/CategoryController.java
+++ src/main/java/br/com/fiap/cash_up_api/controllers/CategoryController.java
@@ -1,23 +1,47 @@
 package br.com.fiap.cash_up_api.controllers;
 
 import br.com.fiap.cash_up_api.models.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CategoryController {
 
+    private List<Category> repository = new ArrayList<>();
+
     @GetMapping("/category")
     public List<Category> index(){
-        return List.of(
-                new Category(1L, "Alimentos", "/assets/food"),
-                new Category(2L, "Lazer", "/assets/casino"),
-                new Category(3L, "Saúde", "/assets/health"),
-                new Category(4L, "Outros", "/assets/others")
-        );
+        return repository;
+    }
+
+    @PostMapping("/category")
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Category> create(@RequestBody Category category){
+        repository.add(category);
+        return ResponseEntity.status(201).body(category);
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<Optional<Category>> show(@PathVariable Long id){
+
+        // optional -> pode existir ou nao
+        Optional<Category> cat = repository.stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst();
+
+        if (!cat.isPresent()) return ResponseEntity.status(404).build();
+
+        return ResponseEntity.status(200).body(cat);
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable Long id){
+        repository.removeIf(category -> category.getId().equals(id));
+        return ResponseEntity.status(204).build();
     }
 }
