0ec0df6c90be464a5549f453bbca53b1d2ab75ed
correialeo <leandroo774@outlook.com>
2025-04-10 09:08:22 -0300

    chore: add specification in controller

--- src/main/java/br/com/fiap/cash_up_api/controllers/TransactionController.java
+++ src/main/java/br/com/fiap/cash_up_api/controllers/TransactionController.java
@@ -2,10 +2,12 @@ package br.com.fiap.cash_up_api.controllers;
 
 import br.com.fiap.cash_up_api.models.Transaction;
 import br.com.fiap.cash_up_api.repositories.TransactionRepository;
+import br.com.fiap.cash_up_api.specification.TransationSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -17,7 +19,7 @@ import java.util.List;
 @Slf4j
 public class TransactionController {
 
-    record TransactionFilter(String description, LocalDate date, BigDecimal amount){}
+    public record TransactionFilter(String description, LocalDate startDate, LocalDate endDate, BigDecimal amount){}
 
     @Autowired
     private TransactionRepository repository;
@@ -25,19 +27,8 @@ public class TransactionController {
     @GetMapping
     public List<Transaction> index(TransactionFilter filter)
     {
-        log.info("Buscando transações com descrição {} e data {}");
-        var probe = Transaction.builder()
-                .description(filter.description)
-                .date(filter.date)
-                .amount(filter.amount)
-                .build();
-
-        var matcher = ExampleMatcher.matchingAll()
-                .withIgnoreCase()
-                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
-
-        var example = Example.of(probe, matcher);
-        return repository.findAll(example);
+        Specification<Transaction> specification = TransationSpecification.withFilters(filter);
+        return repository.findAll(specification);
     }
 
 
